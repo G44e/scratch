77ce66e68643d4f856e81b9f6dc56e76e45e2022
g44e <scala2.8.1@gmail.com>
2011-08-25 13:18:33 +0000

    complete definition

--- src/main/scala/scratch/Fib.scala
+++ src/main/scala/scratch/Fib.scala
@@ -3,7 +3,7 @@ package scratch
 object Fib {
 
     def seq = {
-        val f: Stream[Integer] = 1 #:: 2 #:: (f zip f.tail)
+        val f: Stream[Integer] = 1 #:: 2 #:: ((f zip f.tail) map {case (x, y) => x + y})
         f
     }
 }
